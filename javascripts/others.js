b50468005a18d67df8a751dbbaccae9a672c2130
Wan Saleh <wansaleh@gmail.com>
2014-02-14 21:01:37 +0800

    Some major style change.

--- javascripts/others.js
+++ javascripts/others.js
@@ -2,6 +2,10 @@
 	var verses = $('.mahabbah-quran blockquote');
 	verses.eq(Math.floor(Math.random() * verses.length)).show();
 
+	verses.click(function() {
+		verses.hide().eq(Math.floor(Math.random() * verses.length)).show();
+	});
+
 	// $(function() {
 	// 	var $window = $(window),
 	// 			$body = $('html > body');
@@ -15,4 +19,49 @@
 	// 		$body.css({ backgroundPosition: coords });
 	// 	});
 	// });
+})();
+
+/**
+ * modalEffects by http://www.codrops.com
+ * converted to use jQuery by @wansaleh
+ */
+(function() {
+
+	function init() {
+
+		var doc = $(document.documentElement);
+		var overlay = $('.md-overlay');
+
+		$('.md-trigger').each(function(i) {
+			var el = $(this);
+
+			var modal = $('#' + el.attr('data-modal')),
+				close = modal.find('.md-close');
+
+			function removeModal() {
+				$(modal).removeClass('md-show');
+			}
+
+			function removeModalHandler() {
+				removeModal();
+			}
+
+			el.on('click', function(ev) {
+				modal.addClass('md-show');
+				overlay.off('click', removeModalHandler);
+				overlay.on('click', removeModalHandler);
+				return false;
+			});
+
+			close.on('click', function(ev) {
+				ev.stopPropagation();
+				removeModalHandler();
+			});
+
+		});
+
+	}
+
+	init();
+
 })();
