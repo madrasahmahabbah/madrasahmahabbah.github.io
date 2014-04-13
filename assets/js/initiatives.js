e5dc4acc8ea4083d607320e4e5b8e635b5c2cd88
wansaleh <wansaleh@gmail.com>
2014-04-13 08:01:20 +0800

    Major updates.

---
+++ assets/js/initiatives.js
@@ -0,0 +1,1 @@
+(function(){!function(){var t;return t=$(".footer-hadith"),t.hide().eq(Math.floor(Math.random()*t.length)).show()}()}).call(this);
--- js/initiatives.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){var t;return t=$(".footer-hadith"),t.hide().eq(Math.floor(Math.random()*t.length)).show()})}).call(this);
